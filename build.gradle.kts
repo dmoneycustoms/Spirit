e9d56e4a40f3d58101699fdceaa3b2c9741a6579
N Jones <dmoneycustoms44@gmail.com>
2026-08-14 09:40:41 -0400

    Remove plugin declarations from build.gradle.kts
    Removed plugin declarations for Android and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+    // Root project has no plugins
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
